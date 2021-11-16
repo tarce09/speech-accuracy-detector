c61372c1b9234e42f4303dcd91c7b79465a9df5b
tarce09 <aryanraju09@gmail.com>
2021-11-16 21:33:37 +0530

    2nd commit

--- speech.py
+++ speech.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import Levenshtein as Lev
 
 r=sr.Recognizer()
 
@@ -8,6 +9,10 @@ with sr.AudioFile('audioplay.wav') as source:
 
     try:
         text=r.recognize_google(audio)
+        string='Hello I am Aryan I am 20 I study in MIT'
+        text=text.lower()
+        string=string.lower()
+        print(Lev.distance(text,string))
         print("you said :",text)
     except:
         print("sorry couldnot recognize")
