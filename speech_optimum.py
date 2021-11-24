d2beb4403a69d7038a23f716ce0133bae99c6687
tarce09 <aryanraju09@gmail.com>
2021-11-24 17:47:34 +0530

    optimum code

---
+++ speech_optimum.py
@@ -0,0 +1,28 @@
+from numpy import vectorize
+import speech_recognition as sr
+import Levenshtein as Lev
+import string
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+r=sr.Recognizer()
+
+with sr.AudioFile('audioplay.wav') as source:
+    print("speak now")
+    audio = r.listen(source)
+
+    try:
+        
+        text=r.recognize_google(audio)
+        string='Hello I am Aryan I am 20 I study in MIT'
+        text=text.lower()
+        string=string.lower()
+        sentences=[text,string]
+        vectorizer=CountVectorizer().fit_transform(sentences)
+        vectors=vectorizer.toarray()
+        
+        csim=cosine_similarity(vectors)
+        print(csim[1][0]*100)
+    except:
+        print("sorry couldnot recognize")
